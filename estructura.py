41545a0d8cffe923e4b3d4b22ed26e0980eb979b
Arayve <alejandro150998@outlook.com>
2018-07-01 22:58:36 -0300

    Agregando primitiva de algunas estructura

--- estructura.py
+++ estructura.py
@@ -1,17 +1,30 @@
-"""Clase castillo"""
+class Tipo:
+	def __init__(self,clase):
+		self.clase = clase
 class Castillo:
 	def __init__(self,nombre_castillo,nivel,ubicacion):
+		"""Recibe una cadena, un size_t, un clase grafo"""
 		self.nivel=nivel
 		self.nombre=nombre_castillo
 		self.puntos_resistencia=nivel*100
 		self.ubicacion=ubicacion
-"""Clase aldea"""
+	def ver_aldea_adyacentes(self):
+		"""Revuelve una lista con las aldeas"""
+		lista_aux = []
+		vertice_ady = self.ubicacion.adyacentes_vertice()
+		for vertice in vertice_ady:
+			if vertice.tipo=="ALDEA":
+				lista_aux.append(vertice)
+		return vertice_ady
 class Aldea:
-	def __init__(self,nombre_aldea,cantidad_de_habitante,nivel):
-		self.nombre=nombre_aldea
-		self.poblacion=cantidad_de_habitante
-		self.nivel=nivel
+	def __init__(self,nombre_aldea,cantidad_de_habitante,nivel,impuesto):
+		self.nombre = nombre_aldea
+		self.impuesto = impuesto
+		self.felicidad = 100-impuesto
+		self.poblacion = cantidad_de_habitante
+		self.nivel = nivel
 	def cobrar_impuesto(self):
-		return self.poblacion*self.nivel
+		return self.poblacion*self.impuesto
+
 		
 
